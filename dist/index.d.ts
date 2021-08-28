a70065919f9e1632f92a97e65fe09438ac3b55ea
cairbyte71 <cairzz222@gmail.com>
2021-08-28 12:06:33 +0100

    Delete dist directory

--- dist/index.d.ts
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env node
-import { AnnonfilesAPIHandler } from "./utils/api_calls";
-declare const APIAnnon: AnnonfilesAPIHandler;
-export default APIAnnon;
--- dist/utils/cli_handler.d.ts
+++
@@ -1,2 +0,0 @@
-import { AnnonfilesAPIHandler } from "./api_calls";
-export declare const CLI_Handler: (args: string[], { download, upload, getInfo }: AnnonfilesAPIHandler) => void;
