58bd36873bbec581bb1f9532303e65c349621d99
CA1R7 <cairzz222@gmail.com>
2021-12-01 15:15:49 +0000

    Added help command also updated download command method

--- src/utils/cli_handler.ts
+++ src/utils/cli_handler.ts
@@ -1,16 +1,24 @@
 import { resolve } from "path";
-import { AnnonfilesAPIHandler, AnnonResponseType } from "./api_calls";
+import { AnnonFilesAPIHandler, DownloadOptions, ResponseType, ProtocolsProxies } from "./api_calls";
 
-export const CLI_Handler = (args: string[], { download, upload, getInfo }: AnnonfilesAPIHandler) => {
+export const cli_handler = (args: string[], { download, upload, getInfo }: AnnonFilesAPIHandler) => {
   try {
-    const notifyByConsole = ({ status, data }: AnnonResponseType) => {
-      if (!status || !data) throw new Error("Invalid Data");
+    const notifyByConsole = ({ status, data }: ResponseType) => {
+      if (!status || !data) {
+        throw new Error("Invalid Data");
+      }
       console.log("* File URL -> " + data.file.url.full);
       console.log("* File ID -> " + data.file.metadata.id);
       console.log("* File Size -> " + data.file.metadata.size.readable);
     };
     const run_command = async (command: string, started: number) => {
       switch (command) {
+        case "-h":
+        case "--help":
+          console.log(
+            `Use these commands below:\n  -u, --upload  - for uploading files, Also If you want to use key acces do e.g. -u file@keyacceshere\n  -d, --download  - for get direct download link, Also if you want to use proxy do e.g. -d ID@proxyhttphere\n  -i, --info  - for get info about ID`,
+          );
+          break;
         case "-v":
         case "--version":
           const { version } = require("../../package.json");
@@ -18,38 +26,63 @@ export const CLI_Handler = (args: string[], { download, upload, getInfo }: Annon
           break;
         case "-u":
         case "--upload":
+          const target: string = args[started + 1];
+
           let file: string | null = null;
           let key: string | null = null;
-          const target: string = args[started + 1];
+
           if (target.match(/@/)) {
-            const ar = target.split(/@/);
-            if (ar) {
-              file = ar[0];
-              key = ar[1];
+            const targetCommand = target.split(/@/);
+            if (targetCommand) {
+              file = targetCommand[0]; // file
+              key = targetCommand[1]; // key access
             }
-          } else file = target;
+          } else {
+            file = target;
+          }
 
           if (file) {
-            // double check if file vallid
-            const PATH = resolve(process.cwd(), file); // path
-            const FileUploaded = await upload({
-              file: PATH,
+            // double check if the file vallid
+            const PATH = resolve(process.cwd(), file);
+            const file_uploaded = await upload(PATH, {
               key: key,
             });
-            notifyByConsole(FileUploaded);
+            notifyByConsole(file_uploaded);
           }
           break;
         case "-i":
         case "--info":
         case "-d":
         case "--download":
-          const id = args[started + 1];
+          let id = args[started + 1],
+            proxy: string | undefined,
+            protocol: ProtocolsProxies | null = null;
+
           if (!id) {
             throw new Error("Id is required");
           }
+          // get proxy e.g. XXXXX@00.00.00.00
+          if (id.match(/@/)) {
+            let [pid, iproxy] = id.split(/@/g);
+            id = pid;
+            if (iproxy.match(/\:\/\//g)) {
+              protocol = iproxy.split(/\:\/\//g)[0] as ProtocolsProxies;
+              proxy = iproxy.replace(/(\w+)+\:\/\/|\/(.*)+$/g, "").trim();
+            } else {
+              proxy = iproxy;
+            }
+          }
+
           if (command.indexOf("d") !== -1) {
-            console.log("* Direct download URL -> " + (await download(id)));
-          } else notifyByConsole(await getInfo(id));
+            console.log(
+              `${proxy ? `[${proxy}] ` : ""}* Direct download URL -> ${await download(
+                id,
+                proxy ? { proxy, protocol } : undefined,
+              )}`,
+            );
+          } else {
+            notifyByConsole(await getInfo(id));
+          }
           break;
       }
     };
